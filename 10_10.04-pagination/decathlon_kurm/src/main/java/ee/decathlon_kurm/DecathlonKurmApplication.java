f26d9710e4a3491542e982e4f00d3ed2176415fd
uku@tlu <ukukurm@tlu.ee>
2026-05-15 04:26:01 +0300

    fixed decathlon folder

---
+++ 10_10.04-pagination/decathlon_kurm/src/main/java/ee/decathlon_kurm/DecathlonKurmApplication.java
@@ -0,0 +1,13 @@
+package ee.decathlon_kurm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecathlonKurmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecathlonKurmApplication.class, args);
+	}
+
+}
